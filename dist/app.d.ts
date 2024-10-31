75d29d43ecd7c6ce703f12e9c0ed6ee5d0fd9481
KERIM Abdelmouiz <abdelmouizkerim@gmail.com>
2024-10-31 05:19:19 +0100

    changes(*)

--- dist/app.d.ts
+++ dist/app.d.ts
@@ -1,4 +1,1 @@
-declare const express: any;
-declare const sqlite3: any;
-declare const db: any;
-declare const SqliteGuiNode: any;
+export {};
--- dist/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const express = require("express");
-const sqlite3 = require("sqlite3").verbose();
-// import the SQLite DB that you use
-const db = new sqlite3.Database("app.db");
-// Import the package
-const { SqliteGuiNode } = require("./");
-SqliteGuiNode(db, 3001);
